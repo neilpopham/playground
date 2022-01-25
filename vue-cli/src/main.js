d5fac3dc3a2f35dbbd9f65bed04e15a748f093b1
Neil Popham <neilpopham@gmail.com>
2022-01-25 22:04:39 +0000

    app vs vm

--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+const vm = app.mount('#app');
+
+console.log(app);
+console.log(vm.$data);
+console.log(vm.$data.data1);
+console.log(vm.data2);
