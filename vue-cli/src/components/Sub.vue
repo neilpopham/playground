d2dfcdb69ed89697fb21693894c91d5376ee97b8
Neil Popham <neilpopham@gmail.com>
2022-01-25 21:44:44 +0000

    Tidy tabs

--- vue-cli/src/components/Sub.vue
+++ vue-cli/src/components/Sub.vue
@@ -1,12 +1,12 @@
 <template>
-	<h1>Sub</h1>
-	<h2>prop1: {{ prop1 }}</h2>
-	<h2>prop2: {{ prop2 }}</h2>
-	<p>Slot: <slot /></p>
+  <h1>Sub</h1>
+  <h2>prop1: {{ prop1 }}</h2>
+  <h2>prop2: {{ prop2 }}</h2>
+  <p>Slot: <slot /></p>
 </template>
 
 <script>
 export default {
-	props: ['prop1', 'prop2']
+  props: ['prop1', 'prop2']
 }
 </script>
