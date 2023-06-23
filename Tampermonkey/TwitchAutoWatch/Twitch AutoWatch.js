19ecac79d66e6160b6b38cd549244daeac801288
Neil Popham <neilpopham@gmail.com>
2023-06-23 15:04:06 +0100

    0.2

--- Tampermonkey/TwitchAutoWatch/Twitch AutoWatch.js
+++ Tampermonkey/TwitchAutoWatch/Twitch AutoWatch.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Twitch Stream Auto-Watch
 // @namespace    https://greasyfork.org/en/users/1077259-synthetic
-// @version      0.1
+// @version      0.2
 // @description  Auto-Watches a stream that is offline on page load, as soon as it goes online.
 // @author       @Synthetic
 // @license      MIT
@@ -15,7 +15,7 @@
     'use strict';
 
     // The current version
-    const VERSION = 0.1;
+    const VERSION = 0.2;
 
     // Page element selectors
     const STATUS_OFFLINE = 'div.channel-status-info--offline';
@@ -36,24 +36,22 @@
     };    
 
     const onMutate = function(mutationsList) {
-        mutationsList.forEach(mutation => {
-            const offline = document.querySelectorAll(STATUS_OFFLINE);
-            if (offline.length) {
-                clearTimeout(timeout);
-                if (!waiting) {
-                    console.log('Stream is offline. Waiting');
-                    waiting = true;
-                }
-            }            
-            const online = document.querySelectorAll(STATUS_LIVE);
-            if (online.length) {
-                console.log('Stream is online');
-                if (clickWatch()) {
-                    observer.disconnect();
-                    console.log('Watching');
-                }
+        const offline = document.querySelectorAll(STATUS_OFFLINE);
+        if (offline.length) {
+            clearTimeout(timeout);
+            if (!waiting) {
+                console.log('Stream is offline. Waiting');
+                waiting = true;
             }
-        })
+        }
+        const online = document.querySelectorAll(STATUS_LIVE);
+        if (online.length) {
+            console.log('Stream is online');
+            if (clickWatch()) {
+                observer.disconnect();
+                console.log('Watching');
+            }
+        }
     };
 
     console.log('Loaded at', new Date());
